0fb76055429a4d46997c6562276153a96379f8b2
csh928 <csh928@gmail.com>
2020-10-25 22:09:17 +0900

    get weather infor

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello!!!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { Alert } from "react-native";
+import Loading from "./Loading";
+import * as Location from "expo-location";
+import Axios from "axios";
+const API_KEY = "3f625deebd889f0dce9497e53d4f8b8e";
+//37.6062481 127.0656445
+export default class extends React.Component {
+  state = {
+    isLoading: true,
+  };
+  getWeather = async (latitude, longitude) => {
+    const { data } = await Axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+    );
+    console.log(data);
+  };
+  getLocation = async () => {
+    try {
+      //throw Error();
+      await Location.requestPermissionsAsync();
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+      this.setState({ isLoading: false });
+      // Send to API and get weather
+    } catch (error) {
+      Alert.alert("Can't find you.", "So sad");
+    }
+  };
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
